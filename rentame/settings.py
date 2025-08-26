3d28e2ecf6443168e08c21bff5bcba7ecc2c1d4c
Bikembe <bikembecs@gmail.com>
2025-08-26 03:19:46 -0600

    Update settings.py

--- rentame/settings.py
+++ rentame/settings.py
@@ -81,12 +81,15 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'rentame',
+'USER': 'admin',
+'PASSWORD':'admin',
+'HOST': 'localhost',
+'PORT': '3306',
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
